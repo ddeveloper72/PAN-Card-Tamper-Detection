53a388a261b90e900fc5b770469cd75b284b8da5
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-24 16:48:17 +0100

    added paths for static iamges

--- app/templates/views.py
+++ app/templates/views.py
@@ -1,6 +1,13 @@
 # Import package dependencies
+from flask import app
 from skimage.metrics import structural_similarity
 import imutils
 from PIL import Image
 import requests
 import cv2
+
+
+# Add path to config
+app.config['INITIAL_FILE_UPLOADS'] = 'app/static/uploads'
+app.config['EXISTING_FILE'] = 'app/static/original'
+app.config['GENERATED_FILE'] = 'app/static/generated'

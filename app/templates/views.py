2872a305e78f5d64df2e0ad6368cf18cf2621ef7
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-30 09:57:06 +0100

    added POST method for file upload

--- app/templates/views.py
+++ app/templates/views.py
@@ -22,6 +22,11 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
 
+    # If request is POST, execute
+    if request.method == "POST":
+        file_upload = request.files['file_upload']
+        filename = file_upload.filename
+
 #  Main function
 if __name__ == '__main__':
     app.run(debug=True)
